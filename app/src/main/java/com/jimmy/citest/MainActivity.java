e6e62496f3ab356aaede9939f63c3cbfc79f544d
jimmy <jimmy0251@gmail.com>
2015-09-17 01:15:57 +0530

    Test commit 3

--- app/src/main/java/com/jimmy/citest/MainActivity.java
+++ app/src/main/java/com/jimmy/citest/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-	//Add Test comment2
+	//Add Test comment3
     }
 
     @Override
